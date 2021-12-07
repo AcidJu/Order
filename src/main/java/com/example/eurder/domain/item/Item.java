f89d30cb72910071271a63911c65d872633f7515
AcidJu <70847984+AcidJu@users.noreply.github.com>
2021-12-07 17:54:25 +0100

    Story 2

--- src/main/java/com/example/eurder/domain/item/Item.java
+++ src/main/java/com/example/eurder/domain/item/Item.java
@@ -1,15 +1,83 @@
 package com.example.eurder.domain.item;
 
+import java.util.UUID;
+
 public class Item {
+    private final UUID id;
     private final String name;
     private final String description;
     private final double price;
     private int amount;
 
-    public Item(String name, String description, double price, int amount) {
-        this.name = name;
-        this.description = description;
-        this.price = price;
-        this.amount = amount;
+    public Item(ItemBuilder builder) {
+        this.id = builder.id;
+        this.name = builder.name;
+        this.description = builder.description;
+        this.price = builder.price;
+        this.amount = builder.amount;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public static final class ItemBuilder {
+        private UUID id;
+        private String name;
+        private String description;
+        private double price;
+        private int amount;
+
+        private ItemBuilder() {
+        }
+
+        public static ItemBuilder itemBuilder() {
+            return new ItemBuilder();
+        }
+
+        public ItemBuilder withId(UUID id) {
+            this.id = id;
+            return this;
+        }
+
+        public ItemBuilder withName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public ItemBuilder withDescription(String description) {
+            this.description = description;
+            return this;
+        }
+
+        public ItemBuilder withPrice(double price) {
+            this.price = price;
+            return this;
+        }
+
+        public ItemBuilder withAmount(int amount) {
+            this.amount = amount;
+            return this;
+        }
+
+        public Item build() {
+            return new Item(this);
+        }
     }
 }
